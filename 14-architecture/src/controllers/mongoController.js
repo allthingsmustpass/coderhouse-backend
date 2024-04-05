7d9471e59092c8c64cec202d8cd9854f5561aa66
allthingsmustpass <manducatomas@gmail.com>
2024-04-05 18:05:49 -0300

    Project reorganization: MVC structure, DAO for cart data.

---
+++ 14-architecture/src/controllers/mongoController.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const DB_HOST = "localhost";
+const DB_NAME = "ecommerce";
+const DB_PORT = 8080;
+const USERNAME = "user";
+const KEY = "key";
+
+const configConnection = {
+  url: `mongodb+srv://${USERNAME}:${KEY}@cluster0.llc9gue.mongodb.net/?retryWrites=true&w=majority`,
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+};
+
+const mongoDBconnection = async () => {
+  try {
+    await mongoose.connect(configConnection.url, configConnection.options);
+    console.log(
+      `URL: ${configConnection.url.substring(0, 20)}`);
+    console.log(`${DB_NAME}`);
+    console.log(`Connected!`);
+  } catch (error) {
+    console.log(error);
+
+    throw new Error(error);
+  }
+};
+
+module.exports = {
+  configConnection,
+  mongoDBconnection,
+};
---
+++ 14-architecture/src/services/productManager.js
@@ -0,0 +1,152 @@
+const Product = require('./ProductModel');
+
+class ProductManager {
+    /**
+     * Crea un nuevo gestor de productos.
+     */
+    constructor() {
+        this.productList = [];
+        this.id = 1;
+    }
+
+    /**
+     * Verifica si ya existe un producto con el mismo código.
+     */
+    async checkDuplicateCode(code) {
+        try {
+            const existingProduct = await Product.findOne({ code });
+            return existingProduct !== null;
+        } catch (error) {
+            console.error("Error verificando duplicado de código:", error);
+            return true; // Considera que hay un error como precaución
+        }
+    }
+
+    /**
+     * Añade un nuevo producto.
+     */
+    async addProduct(title, description, code, price, status, stock, category, thumbnails) {
+        try {
+            if (await this.checkDuplicateCode(code)) {
+                throw new Error("Ya existe un producto con el mismo código.");
+            }
+            
+            if (!title || !description || !code || !price || !status || !stock || !category) {
+                throw new Error("Todos los campos son requeridos excepto 'thumbnails'.");
+            }
+
+            const newProduct = new Product({
+                title,
+                description,
+                code,
+                price,
+                status,
+                stock,
+                category,
+                thumbnails,
+                id: this.id
+            });
+
+            await newProduct.save();
+            this.productList.push(newProduct);
+            this.id++;
+
+            return newProduct;
+        } catch (error) {
+            console.error("Error añadiendo producto:", error);
+            return null;
+        }
+    }
+
+    /**
+     * Obtiene todos los productos almacenados.
+     */
+    async getProducts({ limit = 10, page = 1, sort = null, query = null }) {
+        try {
+            let filter = {};
+            if (query) {
+                const [key, value] = query.split(":");
+                filter[key] = value;
+            }
+    
+            let queryOptions = {
+                skip: (page - 1) * limit,
+                limit: limit
+            };
+    
+            if (sort) {
+                let sortField = sort[0] === '-' ? sort.slice(1) : sort;
+                let sortOrder = sort[0] === '-' ? -1 : 1;
+                queryOptions.sort = { [sortField]: sortOrder };
+            }
+    
+            const products = await Product.find(filter, null, queryOptions);
+            const totalProducts = await Product.countDocuments(filter);
+            const totalPages = Math.ceil(totalProducts / limit);
+            const hasPrevPage = page > 1;
+            const hasNextPage = page < totalPages;
+            const prevPage = hasPrevPage ? page - 1 : null;
+            const nextPage = hasNextPage ? page + 1 : null;
+
+            return {
+                products,
+                totalPages,
+                prevPage,
+                nextPage,
+                page,
+                hasPrevPage,
+                hasNextPage
+            };
+        } catch (error) {
+            console.error("Error obteniendo productos:", error);
+            return { products: [], total: 0 };
+        }
+    }
+
+    /**
+     * Obtiene un producto por su ID.
+     */
+    async getProductById(id) {
+        try {
+            const product = await Product.findById(id);
+            console.log(product)
+            return product;
+        } catch (error) {
+            console.error("Error obteniendo producto por ID:", error);
+            return null;
+        }
+    }
+
+    /**
+     * Actualiza un producto existente.
+     */
+    async updateProduct(id, updatedFields) {
+        try {
+            const product = await Product.findByIdAndUpdate(id, updatedFields, { new: true });
+            const index = this.productList.findIndex(product => product.id === id);
+            if (index !== -1) {
+                this.productList[index] = product;
+            }
+            return product;
+        } catch (error) {
+            console.error("Error actualizando producto:", error);
+            return null;
+        }
+    }
+
+    /**
+     * Elimina un producto existente.
+     */
+    async deleteProduct(id) {
+        try {
+            await Product.findByIdAndDelete(id);
+            this.productList = this.productList.filter(product => product.id !== id);
+            return true;
+        } catch (error) {
+            console.error("Error eliminando producto:", error);
+            return false;
+        }
+    }
+}
+
+module.exports = ProductManager;
